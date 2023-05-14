697381cf5a4b233e0a73553d1ac5eb5f4ef1e790
Vicente Cartes <70562074+VCartes@users.noreply.github.com>
2023-05-13 23:34:07 -0400

    Added treesitter, lsp and changed some options

--- init.lua
+++ init.lua
@@ -2,3 +2,11 @@ require("vcartes")
 
 vim.cmd('colorscheme gruvbox')
 vim.cmd('language en_US')
+vim.o.number = true
+vim.o.relativenumber = true
+
+vim.o.tabstop = 4
+vim.o.softtabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = 4
+vim.o.smartindent = true
--- lua/vcartes/packer.lua
+++ lua/vcartes/packer.lua
@@ -4,9 +4,32 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-	-- Packer can manage itself
-	use 'wbthomason/packer.nvim'
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
 
-	use { "morhetz/gruvbox" }
+    use { "morhetz/gruvbox" }
+
+    use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v2.x',
+        requires = {
+            -- LSP Support
+            {'neovim/nvim-lspconfig'},             -- Required
+            {                                      -- Optional
+            'williamboman/mason.nvim',
+            run = function()
+                pcall(vim.cmd, 'MasonUpdate')
+            end,
+        },
+        {'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+        -- Autocompletion
+        {'hrsh7th/nvim-cmp'},     -- Required
+        {'hrsh7th/cmp-nvim-lsp'}, -- Required
+        {'L3MON4D3/LuaSnip'},     -- Required
+    }
+}
 end)
-    
+
