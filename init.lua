cc29539f76a94a0e87b7d83119bd80d2b81bbd32
Vicente <70562074+VCartes@users.noreply.github.com>
2025-04-20 20:42:29 -0400

    Updates

--- init.lua
+++ init.lua
@@ -12,7 +12,6 @@ vim.o.wrap = false
 vim.o.tabstop = 4
 vim.o.softtabstop = 4
 vim.o.shiftwidth = 4
-vim.o.expandtab = 4
 vim.o.smartindent = true
 
 vim.o.scrolloff = 8
--- lua/vcartes/remap.lua
+++ lua/vcartes/remap.lua
@@ -1,6 +1,9 @@
 local util = require("vcartes.util")
 
 vim.g.mapleader = " "
+
 vim.keymap.set("n", "<leader>ex", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>o", util.newLine)
 vim.keymap.set("n", "<leader>O", util.newLineBefore)
+
+vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename)
